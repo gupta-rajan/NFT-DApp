442ec7f35fccd08291f5e2c9b65c35df78e642dc
gupta-rajan <rjngupta22@gmail.com>
2025-04-12 18:34:48 +0530

    update course registration and fee payment

--- contracts/courseRegistration.sol
+++ contracts/courseRegistration.sol
@@ -5,7 +5,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @notice Interface to interact with the IdentityNFT contract.
 interface IIdentityNFT {
-    function identityTokenByOwner(address owner) external view returns (uint256);
+    function identityTokenByOwner(
+        address owner
+    ) external view returns (uint256);
 }
 
 /// @notice Interface to interact with the FeePayment contract.
@@ -16,13 +18,13 @@ interface IFeePayment {
 contract CourseRegistration is Ownable {
     IIdentityNFT public identityNFT;
     IFeePayment public feePayment;
-    
+
     struct Course {
         uint256 courseId;
         string courseName;
         bool isActive;
     }
-    
+
     mapping(uint256 => Course) public courses;
     uint256 public currentCourseId;
 
@@ -32,7 +34,10 @@ contract CourseRegistration is Ownable {
     event StudentRegistered(uint256 courseId, address student);
 
     /// @notice Constructor requires addresses for IdentityNFT and FeePayment.
-    constructor(address _identityNFTAddress, address _feePaymentAddress) Ownable(msg.sender) {
+    constructor(
+        address _identityNFTAddress,
+        address _feePaymentAddress
+    ) Ownable(msg.sender) {
         identityNFT = IIdentityNFT(_identityNFTAddress);
         feePayment = IFeePayment(_feePaymentAddress);
     }
@@ -50,10 +55,35 @@ contract CourseRegistration is Ownable {
     /// - Must have paid the fee for the current cycle.
     function registerCourse(uint256 courseId) public {
         require(courses[courseId].isActive, "Course is not active");
-        require(identityNFT.identityTokenByOwner(msg.sender) != 0, "No valid Identity NFT found");
+        require(
+            identityNFT.identityTokenByOwner(msg.sender) != 0,
+            "No valid Identity NFT found"
+        );
         require(feePayment.isFeePaid(msg.sender), "Fee not paid");
+
+        // Check if the user has already registered
+        for (uint i = 0; i < courseRegistrants[courseId].length; i++) {
+            require(
+                courseRegistrants[courseId][i] != msg.sender,
+                "Already registered for this course"
+            );
+        }
 
         courseRegistrants[courseId].push(msg.sender);
         emit StudentRegistered(courseId, msg.sender);
+    }
+
+    /// @notice Check if a user is already registered for a given course.
+    function isRegistered(
+        uint256 courseId,
+        address user
+    ) public view returns (bool) {
+        address[] memory registrants = courseRegistrants[courseId];
+        for (uint i = 0; i < registrants.length; i++) {
+            if (registrants[i] == user) {
+                return true;
+            }
+        }
+        return false;
     }
 }
